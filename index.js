1949060c88dda6f31a190953e015e66ebf8eead0
Pedro Chavez-nolasco <pedrochavez-nolasco@Pedros-MacBook-Pro-2.local>
2022-09-07 13:27:20 -0700

    fixing ci

--- index.js
+++ index.js
@@ -14,17 +14,16 @@ buttons.map(button => {
                 display.innerText = '';
                 // this makes so that the C clears inputs
                 break;
-            case '←':
-                if (display.innerText) {
-                    display.innerText = display.innerText.slice(0, -1);
-                    
-                }
-                break;
             case '=':
                 try{
                     display.innerText = eval(display.innerText);
-                } catch {
+                }catch{
                     display.innerText = 'Error!';
+                }
+                break;
+            case '←':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
                 }
                 break;
             default:
