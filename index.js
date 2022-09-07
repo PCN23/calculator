fa96fc37fe2df5f46097ba3b4a2f6d4bb285bac7
Pedro Chavez-nolasco <pedrochavez-nolasco@Pedros-MacBook-Pro-2.local>
2022-09-07 13:04:49 -0700

    calculator working clear button working and back button working also

---
+++ index.js
@@ -0,0 +1,28 @@
+let display = document.getElementById('display');
+
+// this returns what looks like an Array, we have to make it into an array
+// let buttons = document.getElementsByClassName('button');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+// now thats it an array  we can map through it
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText = '';
+                // this makes so that the C clears inputs
+                break;
+            case '←':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
+                    
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+                // this will add numbers to the display
+        }
+    });
+});
